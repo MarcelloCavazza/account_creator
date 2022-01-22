23ca96cc6909457682c9464b9ff3a0be4d02e6d7
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-01-22 03:01:06 -0300

    atualizei algumas funcoes

--- include/Functions.php
+++ include/Functions.php
@@ -5,39 +5,77 @@
         //lista os dados do cliente, pesquisando sua ID no banco de dados.
         public function listar($id): array{
             try{
-                $verf_array_dados_cliente = $this->connection->prepare("SELECT * FROM clientes where id_cliente = '{$id}';");
-                $verf_array_dados_cliente->execute();
-                if($verf_array_dados_cliente->rowCount() > 0){
-                    return $verf_array_dados_cliente->fetchAll();   
+                $query = $this->connection->prepare("SELECT * FROM clientes where id_cliente = :id_cliente");
+                $query_parametro = ['id_cliente' =>$id];
+                $query->execute($query_parametro);
+                if($query->rowCount() > 0){
+                    return $query->fetchAll();   
                 }
             }catch(PDOException $e){
-                echo "Incompatibilidade de dados inseridos com a do banco de dados: {$e}";
+                echo "Nao foi possivel listar os dados: {$e}";
             }
         }
+        //funcao para verificar se algum dos valores inseridos p/ criar conta já existem
+        public function verificarSeJaExiste($email,$telefone): int{
+            try{
+                $resultado =0;
+                $query = $this->connection->prepare("SELECT * FROM clientes where email_cliente = :email_cliente");
+                $query_parametro = ['email_cliente'=>$email];
+                $query->execute($query_parametro);
+                if($query->rowCount() > 1){
+                    $resultado += 2;
+                }
+    
+                $query = $this->connection->prepare("SELECT * FROM clientes where telefone_cliente = :telefone_cliente");
+                $query_parametro = ['telefone_cliente'=>$telefone];
+                $query->execute($query_parametro);
+                if($query->rowCount() > 1){
+                    $resultado += 4;
+                }
+                return $resultado;
+            }catch(PDOException $e){
+                echo "Nao foi possivel verificar se já existe email e telefone: {$e}";
+            }
+        }
+        //deleta a conta
+        public function deletarConta($id){
+            try{
+                $query = $this->connection->prepare("DELETE FROM clientes where id_cliente = :id_cliente");
+                $query_parametro = ['id_cliente'=>$id];
+                $query->execute($query_parametro);
+                if($query->rowCount() > 0){
+                    $_SESSION = array();
+                    session_destroy();
+                    return true;
+                }
+            }catch(PDOException $e){
+                echo "Nao foi possivel deletar a conta: {$e}";
+            }
+        }   
         //pega o ID do usuario, usado na tela de login e na tela de cadastro para outras funcoes funcionarem.
         public function getId($usuario,$senha):array{
             try{
-                $verf_array_dados_cliente = $this->connection->prepare("SELECT id_cliente FROM clientes where email_cliente = '{$usuario}' and senha_cliente = '{$senha}';");
+                $verf_array_dados_cliente = $this->connection->prepare("SELECT id_cliente FROM clientes where email_cliente = '{$usuario}' and senha_cliente = '{$senha}'");
                 $verf_array_dados_cliente->execute();
                 if($verf_array_dados_cliente->rowCount() > 0){
                     return $verf_array_dados_cliente->fetchAll();   
                 }
             }catch(PDOException $e){
-                echo "Incompatibilidade de dados inseridos com a do banco de dados: {$e}";
+                echo "Nao foi possivel pegar o Id do usuario atual: {$e}";
             }
         }
         //usada para verificar se existe o email e senha inseridos na tela de login,
         // e aproveita e já deixa a sessao do usuario como logado e poder usar o site.
         public function verificar($usuario,$senha){
             try{
-                $verf_array_dados_cliente = $this->connection->prepare("SELECT * FROM clientes where email_cliente = '{$usuario}' and senha_cliente = '$senha';");
+                $verf_array_dados_cliente = $this->connection->prepare("SELECT * FROM clientes where email_cliente = '{$usuario}' and senha_cliente = '{$senha}'");
                 $verf_array_dados_cliente->execute();
                 if($verf_array_dados_cliente->rowCount() > 0){
                     $_SESSION['logged'] = True;
                     return $verf_array_dados_cliente->fetchAll();   
                 }
             }catch(PDOException $e){
-                echo "Incompatibilidade de dados inseridos com a do banco de dados: {$e}";
+                echo "Nao foi possivel verificar se existe ja a conta na tela de login: {$e}";
             }
         }
         //cadastra o usuario.
@@ -61,7 +99,7 @@
                 return true;
             }
             catch(Exception $e){
-                echo "Erro: {$e}";
+                echo "erro no cadastro: {$e}";
                 return false;
             }
         }
@@ -87,10 +125,9 @@
                     $query->execute($query_parametro);
                     return true;
             }catch(PDOException $e){
-                echo "Incompatibilidade de dados inseridos com a do banco de dados: {$e}";
+                echo "Nao foi possivel atualizar sua conta: {$e}";
                 return false;
             }
         }
-
     }
 ?>
