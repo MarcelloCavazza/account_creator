4c65081db9b0d3f20083eb23c46ff9b7277b70bb
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-01-22 18:41:29 -0300

    mudando detalhe

--- include/aviso_deslogado.php
+++ include/aviso_deslogado.php
@@ -2,5 +2,5 @@
 // mostra mensagem caso um usuario tente acessar
 // uma pagina do site sem estar com sua sessao autenticada!
 $logged = $_SESSION['logged'] ?? NULL;
-if(!$logged) die('Você não está logado!<br>Volte a página inicial e logue<br><a href="index.php">Voltar</a>');
+if(!$logged) die('Você não está logado!<br>Volte a página inicial e logue!<br><a href="index.php">Voltar</a>');
 ?>
